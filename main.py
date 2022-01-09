f27d2be9bac7029c6ec8c010009916768716972b
JakubWilczek98 <jakubwilczek98@gmail.com>
2022-01-09 16:08:22 +0100

    Added k_k matrix aggregation

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ def r_beta(length, beta, element_number):
     k = length * beta
     r = np.zeros((4,1))
     
-    if element_number == "1": #bound 1-2
+    if element_number == 1: #bound 1-2
         r[0] = k
         r[1] = k
         
-    if element_number == "2": #bound 2-3
+    elif element_number == 2: #bound 2-3
         r[1] = k
         r[2] = k
         
-    if element_number == "3": #bound 3-4
+    elif element_number == 3: #bound 3-4
         r[2] = k
         r[3] = k
         
-    if element_number == "4": #bound 4-1
+    elif element_number == 4: #bound 4-1
         r[3] = k
-        r[1] = k
+        r[0] = k
     
     else:
         print("R_beta Error")
@@ -27,34 +27,39 @@ def r_beta(length, beta, element_number):
     return r
         
         
-def k_alfa(length, constatn, element_number):
+def k_alfa(length, constant, element_number):
     a1 = (-2)*length*constant/3
     a2 = (-length)*constant/3
     k_alfa = np.zeros((4,4))
 
-    if element_number == "1": #bound 1-2
-        k_alfa[0,0]
-        k_alfa[0,1]
-        k_alfa[1,0]
-        k_alfa[1,1]
+    if element_number == 1: #bound 1-2
+        k_alfa[0,0] = a1
+        k_alfa[0,1] = a2
+        k_alfa[1,0] = a2
+        k_alfa[1,1] = a1
         
-    if element_number == "2": #bound 2-3
-        k_alfa[1,1]
-        k_alfa[1,2]
-        k_alfa[2,1]
-        k_alfa[2,2]
+    elif element_number == 2: #bound 2-3
+        k_alfa[1,1] = a1
+        k_alfa[1,2] = a2
+        k_alfa[2,1] = a2
+        k_alfa[2,2] = a1
         
-    if element_number == "3": #bound 2-3
-        k_alfa[2,2]
-        k_alfa[2,3]
-        k_alfa[3,2]
-        k_alfa[3,3]
+    elif element_number == 3: #bound 2-3
+        k_alfa[2,2] = a1
+        k_alfa[2,3] = a2
+        k_alfa[3,2] = a2
+        k_alfa[3,3] = a1
         
-    if element_number == "4": #bound 4-1
-        k_alfa[0,0]
-        k_alfa[0,3]
-        k_alfa[3,0]
-        k_alfa[3,3]
+    elif element_number == 4: #bound 4-1
+        k_alfa[0,0] = a1
+        k_alfa[0,3] = a2
+        k_alfa[3,0] = a2
+        k_alfa[3,3] = a1
+        
+    else:
+        print(element_number)
+        print("k_alfa error")
+        
         
     return k_alfa
 
@@ -65,5 +70,20 @@ def r_q(a,b,q):
     return r_q
 
 
+def k_k(a,b,k_x,k_y):
+    k_k = np.array([ 
+        (k_y*a/(3*b)+k_x*b/(3*a),  k_y*a/(6*b)-k_x*b/(3*a), -k_y*a/(6*b)-k_x*b/(6*a),  k_x*b/(6*a)-k_y*a/(3*b)),
+        (k_y*a/(6*b)-k_x*b/(3*a),  k_y*a/(3*b)+k_x*b/(3*a),   k_x*b/(6*a)-k_y*a/(3*b), -k_x*b/(6*a)-k_y*a/(6*b)),
+       (-k_y*a/(6*b)-k_x*b/(6*a), -k_y*a/(3*b)+k_x*b/(6*a),   k_y*a/(3*b)+k_x*b/(3*a), -k_x*b/(3*a)+k_y*a/(6*b)),
+       (-k_y*a/(3*b)+k_x*b/(6*a), -k_y*a/(6*b)-k_x*b/(6*a),   k_y*a/(6*b)-k_x*b/(3*a),  k_x*b/(3*a)+k_y*a/(3*b))
+      ])
+    return k_k
 
+if __name__ == '__main__':
+
+
+    
+    
+    
+    
     
